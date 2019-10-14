a9ed685f169b57c1b74fe0aed3165fa206f49fb3
Mohammadreza.Kaloukh <Mohammadreza.Kaloukh@freenet.ag>
2019-10-14 10:28:41 +0200

    Adding two Method strReplaceMark and isValidFilename respectively.

--- src/StrMan.java
+++ src/StrMan.java
@@ -1,3 +1,17 @@
 public class StrMan {
+    //An Example for ==> String what="\\*,\\<,\\>,\\-,\\\\,\\!,\\ ,\\\",\\/,\\',\\:,\\;,\\[,\\]";
+    public static String strRepalcerMark(String originText,String what,String with)
+    {		return originText.replaceAll("["+what+"]", with).trim();
+    }
 
+    public static boolean isValidFilename(String fileName)
+    {final char[] badChars = { '/', '\n', '\r', '\t', '\0', '\f', '`', '?', '*', '\\', '<', '>', '|', '\"', ':' };
+        if (fileName==null ||  fileName.isEmpty()|| fileName.charAt(0)==' ')
+            return false;
+        else
+            for(int counter=0;counter<badChars.length;counter++)
+                if (fileName.indexOf(badChars[counter])>0)
+                    return false;
+        return true;
+    }
 }
